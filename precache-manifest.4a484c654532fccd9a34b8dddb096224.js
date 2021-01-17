d3b14a9f5c3c396108ad5461d3f1f649e40bcc51
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-17 16:19:33 +0000

    Deploy aimanaziz1993/dev to github.com/aimanaziz1993/dev.git:gh-pages

---
+++ precache-manifest.4a484c654532fccd9a34b8dddb096224.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "afe3c121a57b7797595f",
+    "url": "/dev/static/css/main.42fc7619.chunk.css"
+  },
+  {
+    "revision": "afe3c121a57b7797595f",
+    "url": "/dev/static/js/main.afe3c121.chunk.js"
+  },
+  {
+    "revision": "71ef5f2aad4a709438b6",
+    "url": "/dev/static/js/runtime~main.71ef5f2a.js"
+  },
+  {
+    "revision": "06d2d882dbb4e13bac4e",
+    "url": "/dev/static/js/2.06d2d882.chunk.js"
+  },
+  {
+    "revision": "566f51b3293b1623903782f5dad28cba",
+    "url": "/dev/index.html"
+  }
+];
